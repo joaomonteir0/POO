573840f7a5a2396b02c354dcec4ad18df80eabfa
João Monteiro <73401053+joaomonteir0@users.noreply.github.com>
2022-03-18 14:50:05 +0000

    4

---
+++ Aula3/ex4.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class ex4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double ctr = 0, n, max = 0, min = 0, sum = 0;
+        int count = 1;
+
+        while (true) {
+            System.out.print("Número Real? ");
+            n = sc.nextDouble();
+            sum=sum+n;
+
+            if (ctr == 0){
+                ctr = n;
+                max = n;
+                min = n;
+            } else {
+                if (ctr == n) {
+                    break;
+                }
+            }
+            double newmax = Math.max(n, max);
+            double newmin = Math.min(n, min);
+            max = newmax;
+            min = newmin;
+            count++;
+            
+        }
+        double average = sum/count;
+        System.out.println(count);
+        System.out.println(min);
+        System.out.println(max);
+        System.out.println(average);
+
+        sc.close();
+
+    }
+}
